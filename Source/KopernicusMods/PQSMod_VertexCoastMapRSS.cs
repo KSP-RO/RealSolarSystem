5e5f4412609f1b34a6b7d2919378f3f5807b06d6
Richard Crockford <40358880+RCrockford@users.noreply.github.com>
2020-07-16 11:27:19 +0100

    Coast smoothing stuff

---
+++ Source/KopernicusMods/PQSMod_VertexCoastMapRSS.cs
@@ -0,0 +1,70 @@
+﻿/* 
+ * This code is adapted from KopernicusExpansion-Continued
+ * Available from https://github.com/StollD/KopernicusExpansion-Continued
+ */
+
+using System;
+using UnityEngine;
+
+namespace RealSolarSystem
+{
+    /// <summary>
+    /// A heightmap PQSMod that can parse encoded 16bpp textures
+    /// </summary>
+    public class PQSMod_VertexCoastMapRSS : PQSMod_VertexHeightMap
+    {
+        private double[] xcoeffs = new double[4];
+        private double[] ycoeffs = new double[4];
+
+        public override void OnVertexBuildHeight(PQS.VertexBuildData data)
+        {
+            try
+            {
+                // Lanczos resample
+                double xpos = data.u * (heightMap.Width - 1);
+                double ypos = data.v * (heightMap.Height - 1);
+                for (int i = -1; i <= 2; ++i)
+                {
+                    double x = xpos - (Math.Floor(xpos) + i);
+                    if (x == 0.0)
+                        xcoeffs[i + 1] = 1.0;
+                    else if (x >= -2.0 && x < 2)
+                        xcoeffs[i + 1] = -2.0 * Math.Sin(Math.PI * x) * Math.Sin(Math.PI * x / -2) / (Math.PI * Math.PI * x * x);
+                    else
+                        xcoeffs[i + 1] = 0;
+
+                    double y = ypos - (Math.Floor(ypos) + i);
+                    if (y == 0.0)
+                        ycoeffs[i + 1] = 1.0;
+                    else if (y >= -2.0 && y < 2)
+                        ycoeffs[i + 1] = -2.0 * Math.Sin(Math.PI * y) * Math.Sin(Math.PI * y / -2) / (Math.PI * Math.PI * y * y);
+                    else
+                        ycoeffs[i + 1] = 0;
+                }
+
+                // Get the height data from the terrain
+                double height = 0.0;
+                double sum = 0.0;
+                for (int y = -1; y <= 2; ++y)
+                {
+                    for (int x = -1; x <= 2; ++x)
+                    {
+                        double h = heightMap.GetPixelFloat((int)xpos + x, (int)ypos + y);
+                        h *= xcoeffs[x + 1] * ycoeffs[y + 1];
+                        sum += xcoeffs[x + 1] * ycoeffs[y + 1];
+                        height += h;
+                    }
+                }
+
+                height /= sum;
+
+                // Apply it
+                data.vertHeight += heightMapOffset + heightMapDeformity * height;
+            }
+            catch (NullReferenceException e)
+            {
+                Debug.LogError("Caught NRE in VertexCoastMapRSS");
+            }
+        }
+    }
+}
---
+++ Source/KopernicusMods/VertexDefineCoastSmooth.cs
@@ -0,0 +1,39 @@
+﻿/* 
+ * This code is adapted from KopernicusExpansion-Continued
+ * Available from https://github.com/StollD/KopernicusExpansion-Continued
+ */
+
+using System;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.Configuration.ModLoader;
+
+namespace RealSolarSystem
+{
+    public class VertexDefineCoastSmooth : ModLoader<PQSMod_VertexDefineCoastSmooth>
+    {
+        // Height map offset
+        [ParserTarget("minOffset")]
+        public NumericParser<Double> minHeightOffset
+        {
+            get { return Mod.minHeightOffset; }
+            set { Mod.minHeightOffset = value; }
+        }
+
+        // Height map offset
+        [ParserTarget("maxOffset")]
+        public NumericParser<Double> maxHeightOffset
+        {
+            get { return Mod.maxHeightOffset; }
+            set { Mod.maxHeightOffset = value; }
+        }
+
+        // Height map offset
+        [ParserTarget("slopeScale")]
+        public NumericParser<Double> slopeScale
+        {
+            get { return Mod.slopeScale; }
+            set { Mod.slopeScale = value; }
+        }
+    }
+}
